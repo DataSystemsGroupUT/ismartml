c57c51d8df56f139ab2dca07657f5f1d294b38a6
Ubuntu <ubuntu@ip-172-31-42-179.eu-central-1.compute.internal>
2019-07-16 09:22:17 +0000

    welp

--- main.py
+++ main.py
@@ -11,15 +11,16 @@ from multi import run_task
 
 #ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 ALLOWED_EXTENSIONS = set(["npy","csv"])
-CLASSIFIERS=["adaboost","bernoulli_nb","decision_tree", "extra_trees","gaussian_nb", "gradient_boosting","k_nearest_neighbors", "lda","liblinear_svc","libsvm_svc"," 	multinomial_nb","passive_aggressive","qda","random_forest","sgd","xgradient_boosting"]
 
+CLASSIFIERS=["adaboost","bernoulli_nb","decision_tree", "extra_trees","gaussian_nb", "gradient_boosting","k_nearest_neighbors", "lda","liblinear_svc","libsvm_svc"," 	multinomial_nb","passive_aggressive","qda","random_forest","sgd","xgradient_boosting"]
+REGRESSORS=["adaboost","ard_regression","decision_tree", "extra_trees","gaussian_process", "gradient_boosting","k_nearest_neighbors","liblinear_svr","libsvm_svr","qda","random_forest","sgd","xgradient_boosting"]
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
     
 @app.route('/')
 def upload_form():
-    return render_template('upload.html')
+    return render_template('upload.html', CLASSIFIERS=CLASSIFIERS)
 
 @app.route('/', methods=['POST'])
 def upload_file():
