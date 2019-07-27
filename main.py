9c74e244d8d1b08ac663327484431c5fcddfb4a5
Ubuntu <ubuntu@ip-172-31-42-179.eu-central-1.compute.internal>
2019-07-27 21:33:18 +0000

    refactoring

--- main.py
+++ main.py
@@ -128,8 +128,7 @@ def progress():
     format_period=format_time(values["period"])
     
     estimator=run_task(os.path.join(app.config['UPLOAD_FOLDER'], values["filename"]),values["task"],values["data_type"])
-    print("time: ",values["time"])
-    results=estimator(values["turn"]+1,values["period"],values["search_space"],values["prep_space"])
+    results=estimator(values["turn"],values["period"],values["search_space"],values["prep_space"])
     df=pd.DataFrame(data=results).sort_values(by="rank_test_scores")
     col_names=["Score","Estimator","Preprocessing","Details"]
     res_list = [[a,b]for a, b in zip(df["mean_test_score"].values.tolist(),df["params"].values.tolist())]
@@ -137,15 +136,13 @@ def progress():
     filehandler = open("tmp/results.p", 'wb') 
     pickle.dump(res_list, filehandler)
     
-    print(values["turn"])
     values["turn"]=values["turn"]+1
-    print(values["turn"])
     session["values"]=values
     if(values["task"]=="classification"):
         res_list=[[row[0],row[1]["classifier:__choice__"],row[1]["preprocessor:__choice__"],"view"] for row in res_list]
     else:
         res_list=[[row[0],row[1]["regressor:__choice__"],row[1]["preprocessor:__choice__"],"view"] for row in res_list]
-    if(values["turn"]>=iters-1):
+    if(values["turn"]>=iters):
         return render_template("results.html",column_names=col_names, row_data=res_list,zip=zip)
     else:
         return render_template("progress.html",turn=values["turn"],iters=iters,PERIOD=format_period,task=values["task"],time=values["time"],column_names=col_names, row_data=res_list,zip=zip)
