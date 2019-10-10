cc8759192bc290c3da5e1dd19f03404ce688cb96
Ubuntu <ubuntu@shota.cloud.ut.ee>
2019-10-10 09:04:57 +0000

    confusion

--- pipeline_gen.py
+++ pipeline_gen.py
@@ -29,10 +29,15 @@ from autosklearn.pipeline.components.classification.libsvm_svc import LibSVM_SVC
 
 
 import pandas as pd
+import numpy as np
 from sklearn import datasets
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
 
 
 # In[34]:
@@ -235,6 +240,61 @@ def get_importance(pipe,cl,smote):
     return []
 
 
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    #classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    #print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    fig.savefig("static/images/figs/test",bbox_inches="tight",transparent=True)
+    return ax
+
+
 def get_matrix(pipe,X,y,smote):
     trees=["gradient_boosting","decision_tree","random_forest","extra_trees","adaboost"]
     linear=["libsvm_svc"]
@@ -242,6 +302,7 @@ def get_matrix(pipe,X,y,smote):
     if smote=="yes":
         ind=2
     pred_y=pipe.steps[ind][1].predict(X) 
+    plot_confusion_matrix(y,pred_y,np.unique(y))
     return confusion_matrix(y,pred_y)
 
 
