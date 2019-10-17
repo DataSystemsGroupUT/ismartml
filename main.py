f22d22101e6656a990f2371a6fd2df75474c175c
Ubuntu <ubuntu@shota.cloud.ut.ee>
2019-10-17 10:30:39 +0000

    not good

--- main.py
+++ main.py
@@ -371,24 +371,25 @@ def progress():
     with open("tmp/results.p", 'rb') as filehandler:
         or_list=pickle.load(filehandler)
     
-    index=CLASSIFIERS[CLASSIFIERS_DISP.index(res_list[0][0])]
-    #index = request.args.get('model', default = None, type = str)
-    fres_list=or_list[index]
-    slc=len("classifier:{}:".format(index))
-    col_names_e=[x for x in list(fres_list[0][1].keys()) if x[:10]=="classifier" and x[-21:]!="min_impurity_decrease"][1:]
-    fres_list=[[round(x[0],3),x[1]["preprocessor:__choice__"].replace("_"," ").title()]+ [x[1][k]  if type(x[1][k])!= float  and type(x[1][k])!=str else round(x[1][k],3) if type(x[1][k])==float else x[1][k].replace("_"," ").title() for k in  col_names_e ]+["Interpret"] for x in fres_list]
-    col_names_e= [("{} Score".format(values["metric"])),"Preprocessor"]+[x[slc:].replace("_"," ").title() for x in col_names_e]+["Details"]
-    disp_index=index.replace("_"," ").title()
-    ##plotting
-    fig_names=[]
-    for i in range(1,len(res_list[0])):
-        if type(res_list[0][i])==float or type(res_list[0][i])==int:
-            plt.clf()
-            plt.xlabel(col_names[i])
-            plt.ylabel("{} Score".format(values["metric"]))
-            plt.scatter([x[i] for x in res_list],[x[0] for x in res_list])
-            plt.savefig("static/images/figs/"+index+str(i),bbox_inches="tight",transparent=True)
-            fig_names.append(index+str(i))
+    for each in res_list:
+        index=CLASSIFIERS[CLASSIFIERS_DISP.index(each[0])]
+        #index = request.args.get('model', default = None, type = str)
+        fres_list=or_list[index]
+        slc=len("classifier:{}:".format(index))
+        col_names_e=[x for x in list(fres_list[0][1].keys()) if x[:10]=="classifier" and x[-21:]!="min_impurity_decrease"][1:]
+        fres_list=[[round(x[0],3),x[1]["preprocessor:__choice__"].replace("_"," ").title()]+ [x[1][k]  if type(x[1][k])!= float  and type(x[1][k])!=str else round(x[1][k],3) if type(x[1][k])==float else x[1][k].replace("_"," ").title() for k in  col_names_e ]+["Interpret"] for x in fres_list]
+        col_names_e= [("{} Score".format(values["metric"])),"Preprocessor"]+[x[slc:].replace("_"," ").title() for x in col_names_e]+["Details"]
+        disp_index=index.replace("_"," ").title()
+        ##plotting
+        fig_names=[]
+        for i in range(1,len(res_list[0])):
+            if type(res_list[0][i])==float or type(res_list[0][i])==int:
+                plt.clf()
+                plt.xlabel(col_names[i])
+                plt.ylabel("{} Score".format(values["metric"]))
+                plt.scatter([x[i] for x in res_list],[x[0] for x in res_list])
+                plt.savefig("static/images/figs/"+index+str(i),bbox_inches="tight",transparent=True)
+                fig_names.append(index+str(i))
     
 
 
