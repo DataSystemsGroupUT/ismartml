138266638676b9a680cf748e82d1d8e8c2bdf7bf
shota <shamashukeli@gmail.com>
2019-11-14 15:29:04 +0000

    fixed formatting

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from key import key
 
 UPLOAD_FOLDER = 'uploads'
 
 app = Flask(__name__)
-app.secret_key = "secret key"
+app.secret_key = key
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
