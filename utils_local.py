7d4aa93e3c73d1b25134fa00a45141e961b75242
shota <shamashukeli@gmail.com>
2019-11-15 13:47:25 +0000

    Fixing PEP8 standards

--- utils_local.py
+++ utils_local.py
@@ -1,19 +1,21 @@
-import pandas as pd
+""" Utility functions used by the tool """
 from hashlib import md5
+import pandas as pd
 
 def hash_file(path):
-    chk= md5()
-    with open(path,'rb') as f:
+    """ Returns md5 hash of a file"""
+    chk = md5()
+    with open(path, 'rb') as f:
         for chunk in iter(lambda: f.read(4096), b""):
             chk.update(chunk)
     return chk.hexdigest()
 
 def return_cols(path):
-    data=pd.read_csv(path)
+    """ Returns column names of the CSV"""
+    data = pd.read_csv(path)
     return list(data.columns)
 
-def select_cols(path,cols):
-    data=pd.read_csv(path)
+def select_cols(path, cols):
+    """Select passed columns from the CSV"""
+    data = pd.read_csv(path)
     return data[data.columns.intersection(cols)]
-
-
