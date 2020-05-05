0a6e99f9c26e0e8e5de2c47ee5c3f33bda6c420c
Shota Amashukeli <shamashukeli@gmail.com>
2020-05-05 20:36:07 +0000

    about

--- main.py
+++ main.py
@@ -34,6 +34,10 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit(
         '.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 
 @app.route('/')
 def start():
